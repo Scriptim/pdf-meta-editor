1c3e5dac3884d52208f47a693cdfaface22d96ea
Scriptim <Scriptim@gmx.de>
2018-07-14 00:25:36 +0200

    Add option -h/--help option to show help page

--- index.js
+++ index.js
@@ -6,13 +6,30 @@ const metadataPrinter = require('./metadata_printer.js')
 
 const argv = require('minimist')(process.argv.slice(2))
 
+const helpPage = `pdf-meta-editor
+
+Usage: pdf-meta-editor FILENAME
+       pdf-meta-editor (-h|-v)
+       pdf-meta-editor -p FILENAME
+
+options
+  -h --help     Show this help page.
+  -v --version  Print the currently installed version.
+  -p --print    Print the metadata instead of editing it.
+`
+
+if (argv.h === true || argv.help === true) {
+  process.stdout.write(helpPage)
+  process.exit(0)
+}
+
 if (argv.v === true || argv.version === true) {
   process.stdout.write(`${process.version}\n`)
   process.exit(0)
 }
 
 if (argv._.length !== 1) {
-  process.stderr.write('Usage: pdf-meta-editor [options] FILENAME\n')
+  process.stdout.write(helpPage)
   process.exit(1)
 }
 
@@ -22,3 +39,4 @@ if (argv.p === true || argv.print === true) {
 } else {
   metadataEditor.useFile(filename)
 }
+
