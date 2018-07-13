6a69c8aaec940a8c4b80bd0016eb419429d5318a
Scriptim <Scriptim@gmx.de>
2018-07-13 03:39:32 +0200

    Get filename from command line arguments

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 const metadataEditor = require('./metadata_editor.js')
+
+if (process.argv.length !== 3) {
+  console.log('Usage: pdf-meta-editor FILENAME')
+  process.exit(1)
+}
+
+const filename = process.argv[2]
